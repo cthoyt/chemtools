1034e53a846e6a5211654bdc349c35b24744b876
Charlie Hoyt <cthoyt@gmail.com>
2014-02-12 16:39:23 -0500

    splitting predictor
    a fun tool for predicting first order hnmr coupling patterns

---
+++ splitting.py
@@ -0,0 +1,51 @@
+def makeSplittingList(l):
+    # input list of doubles as (number hydrogens, coupling constant in hz)
+    # output list of doubles as (relative coordinate, peak height)
+    # produces a first-order coupling tree centered at zero with ratios of heights
+
+	splits = []
+	for h, j in l:
+		splits.extend(j for i in range(h))
+
+	coordinates = [0]
+	for j in splits:
+		nc = []
+		for coordinate in coordinates:
+			nc.extend(rangeList(coordinate - j / 2.0, coordinate + j / 2.0, j))
+		coordinates = nc
+
+	answer = {}
+	for s in coordinates:
+			answer[s] = 1 if s not in answer else answer[s] + 1
+
+	return answer
+
+def test_makeSplittingList():
+	test1 = ([(1, 5)], {-2.5: 1, 2.5: 1})
+	test2 = ([(1, 4)], {-2: 1, 2: 1})
+	test3 = ([(2, 4)], {-4: 1, 0: 2, 4: 1})
+	test4 = ([(2, 5)], {-5: 1, 0: 2, 5: 1})
+	test5 = ([(3, 4)], {-6: 1, -2: 3, 2: 3, 6: 1})
+	test6 = ([(3, 5)], {-7.5: 1, -2.5: 3, 2.5: 3, 7.5: 1})
+	test7 = ([(1, 4), (1, 2)], {-3: 1, -1: 1, 1: 1, 3: 1})
+	data = [test1, test2, test3, test4, test5, test6, test7]
+	for test, result in data:
+		assert makeSplittingList(test) == result
+
+def rangeList(start, stop, step):
+	l = []
+	while start <= stop:
+		l.append(start)
+		start += step
+	return l
+
+def test_rangeList():
+	test1 = (0, 3, 1, [0, 1, 2, 3])
+	test2 = (-2.5, 2.5, 5, [-2.5, 2.5])
+	test3 = (-7.5, 7.5, 5, [-7.5, -2.5, 2.5, 7.5])
+	data = [test1, test2, test3]
+	for start, stop, step, result in data:
+		assert rangeList(start, stop, step) == result
+
+test_rangeList()
+test_makeSplittingList()
