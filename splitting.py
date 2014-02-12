2ef6bb80d982d579dd52fcdea92ed7d8d784e17d
Charlie Hoyt <cthoyt@gmail.com>
2014-02-12 18:00:28 -0500

    better docs and tests

--- splitting.py
+++ splitting.py
@@ -5,12 +5,16 @@ def makeSplittingList(l):
     # output list of doubles as (relative coordinate, peak height)
     # produces a first-order coupling tree centered at zero with ratios of heights
 
+	# count the number of each type of splitting
 	splits = []
 	for num_hydrogen, j in l:
 		splits.extend(j for i in xrange(num_hydrogen))
-	
-	shifts = reduce(lambda shifts, j: list(itertools.chain(*[rangeList(shift - j / 2.0, shift + j / 2.0, j) for shift in shifts])), splits, [0])
 
+	# challenge: recursive implementation (because why not)
+	# populate a list of the locations of each split
+	shifts = reduce(lambda shifts, j: itertools.chain(*[rangeList(shift - j / 2.0, shift + j / 2.0, j) for shift in shifts]), splits, [0])
+
+	# count the frequency at each point as relative heights
 	answer = {}
 	for s in shifts:
 			answer[s] = 1 if s not in answer else answer[s] + 1
@@ -25,7 +29,8 @@ def test_makeSplittingList():
 	test5 = ([(3, 4)], {-6: 1, -2: 3, 2: 3, 6: 1})
 	test6 = ([(3, 5)], {-7.5: 1, -2.5: 3, 2.5: 3, 7.5: 1})
 	test7 = ([(1, 4), (1, 2)], {-3: 1, -1: 1, 1: 1, 3: 1})
-	data = [test1, test2, test3, test4, test5, test6, test7]
+	test8 = ([(1, 8), (1, 4), (1,2)], {key: 1 for key in rangeList(-7.0,7.0,2.0)})
+	data = [test1, test2, test3, test4, test5, test6, test7, test8]
 	for test, result in data:
 		assert makeSplittingList(test) == result
 
